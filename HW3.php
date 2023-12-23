23fb9b70fd31c12bea5a108852349a5f2aa2d3ad
nonthawattabanhan <65160225@go.buu.ac.th>
2023-12-23 14:26:35 +0700

    HW3

--- HW3.php
+++ HW3.php
@@ -3,13 +3,36 @@
 
 <head>
     <meta charset="utf-8">
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background: url('https://img.pikbest.com/backgrounds/20190424/bright-stars-background-clouds-texture-background_2820051.jpg!w700wp') center/cover fixed;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        div {
+            width: 150px;
+            height: 265px;
+            overflow: auto;
+            background-color: rgba(255, 255, 255, 0.8); 
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+    </style>
 </head>
 
 <body>
     <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
-    <div style="width: 150px; height: 265px; overflow: auto;">
+    <div>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
         <table>
             <tbody>
                 <tr>
@@ -32,15 +55,8 @@
                 }
                 ?>
             </tbody>
-            mmll
         </table>
     </div>
-
-    <style>
-        table {
-            width: 100%;
-        }
-    </style>
 </body>
 
 </html>
